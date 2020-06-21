4e837e333b66a68d0cd674ded60d6bbecf62929c
Adam Boettcher <baboettcher@gmail.com>
2020-06-21 14:54:29 -0700

    test 4

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ require("./startup/validation")();
 
 const port = process.env.PORT || config.get("port");
 const server = app.listen(port, () => {
-  console.log(`Listening on port ${port}...`)
+  console.log(`Listening HERE on port ${port}...`)
   winston.info(`Listening on port ${port}...`)
   return
 }
